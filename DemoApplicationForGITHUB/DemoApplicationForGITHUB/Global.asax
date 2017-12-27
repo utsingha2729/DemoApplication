9604130c0199f260cb01758bae860a04387eb042
Zoltar <34880979+utsingha2729@users.noreply.github.com>
2017-12-27 14:13:32 +0530

    Version Updated

---
+++ DemoApplicationForGITHUB/DemoApplicationForGITHUB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoApplicationForGITHUB.MvcApplication" Language="C#" %>
---
+++ DemoApplicationForGITHUB/DemoApplicationForGITHUB/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoApplicationForGITHUB.Models
+{
+	public class Employee
+	{
+		public int EmployeeID { get; set; }
+		public string EmployeeName { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		public DateTime DateOfJoining { get; set; }
+	}
+}
